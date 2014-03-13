45865b8cf5e3d6bbc5e3b4d2dacc020322457e02
mzdravkov <mihail0zdravkov@gmail.com>
2014-03-13 23:33:01 +0200

    Somewhat playable sokoban. It now has textures and you can move and push boxes (although there are few bugs)

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -1,40 +1,144 @@
-function draw() {
-  var canvas = document.getElementById("field");
-  if (!(canvas.getContext == null)) {
-    var context = canvas.getContext("2d");
-    context.fillStyle = "rgb(150, 29, 28)";
-    context.fillRect(10, 10, 100, 100);
-    return null;
-  } else {
-    return alert("This pages uses HTML5 to render correctly. Be modern and stop using shitty browsers. Please.");
-  }
-};
-
 function shit() {
   GetCellValues("level");
 }
 
+function showImage(where, src, width, height) {
+    var img = document.createElement("img");
+    img.src = src;
+    img.width = width;
+    img.height = height;
+
+    where.appendChild(img);
+}
+
+var player_pos = null;
+var lvl = Array();
+
 function GetCellValues(from) {
   var table = document.getElementById(from);
   for (var r = 0, n = table.rows.length; r < n; r++) {
+    lvl.push([]);
     for (var c = 0, m = table.rows[r].cells.length; c < m; c++) {
-      //alert(table.rows[r].cells[c].innerHTML);
-      var cell = table.rows[r].cells[c]
-      if (cell.firstChild.firstChild.data == "e") {
-        cell.firstChild.firstChild.data = " ";
-      }
-      cell.firstChild.style.width = "20px";
-      cell.firstChild.style.height = "20px";
+      var cell = table.rows[r].cells[c];
+      lvl[lvl.length - 1].push(cell.firstChild.innerHTML);
       switch (cell.firstChild.innerHTML) {
-        case 'w': cell.firstChild.style.backgroundColor = "black"; break;
-        case 'e': cell.firstChild.style.backgroundColor = "white"; break;
-        case 'g': cell.firstChild.style.backgroundColor = "purple"; break;
-        case 'c': cell.firstChild.style.backgroundColor = "yellow"; break;
-        case 'o': cell.firstChild.style.backgroundColor = "white";
-                  cell.firstChild.firstChild.data = 'o';
+        case 'w': addImg("wall.jpg", cell.firstChild.id, "42px"); break;
+        case 'e': addImg("ground.jpg", cell.firstChild.id, "42px"); break;
+        case 'g': addImg("ground.jpg", cell.firstChild.id, "42px");
+                  cell.firstChild.style.width = "40px";
+                  cell.firstChild.style.height = "40px";
+                  cell.firstChild.style.border = "1px solid red";
                   break;
-        case 'gc': cell.firstChild.style.backgroundColor = "gold"; break;
+        case 'c': addImg("crate.png", cell.firstChild.id, "42px"); break;
+        case 'p': addImg("player2.png", cell.firstChild.id, "42px");
+                  player_pos = [c, r];
+                  break;
+        case 'gp': addImg("player2.png", cell.firstChild.id, "42px");
+                   player_pos = [c, r];
+                   break;
+        case 'gc': addImg("crate.png", cell.firstChild.id, "42px");
+                   cell.firstChild.style.width = "40px";
+                   cell.firstChild.style.height = "40px";
+                   cell.firstChild.style.border = "1px solid gold";
+                   break;
+      }
+      cell.firstChild.firstChild.data = " ";
+    }
+  }
+}
+
+function addImg(img, srcId, size) {
+  var src = document.getElementById(srcId);
+  src.style.backgroundImage = "url(images/" + img + ")";
+  src.style.backgroundSize = size;
+}
+
+document.onkeypress = khandle;
+
+function khandle(e) {
+  var char = String.fromCharCode(e.keyCode || e.charCode);
+  switch(char) {
+    case '&': move([0, -1]); //top
+      break;
+    case '%': move([-1, 0]); //left
+      break;
+    case '(': move([0, 1]); //down
+      break;
+    case '\'': move([1, 0]); //right
+      break;
+    case 'r': alert("restart")
+      break;
+  }
+}
+
+function refresh() {
+  for (var r = 0; r < lvl.length; r++) {
+    for (var c = 0; c < lvl[r].length; c++) {
+      var cellDiv = document.getElementById(r + "_" + c);
+      switch (lvl[r][c]) {
+        case 'w': addImg("wall.jpg", cellDiv.id, "42px"); break;
+        case 'e': addImg("ground.jpg", cellDiv.id, "42px"); break;
+        case 'g': addImg("ground.jpg", cellDiv.id, "42px");
+                  cellDiv.style.width = "40px";
+                  cellDiv.style.height = "40px";
+                  cellDiv.style.border = "1px solid red";
+                  break;
+        case 'c': addImg("crate.png", cellDiv.id, "42px"); break;
+        case 'p': addImg("player2.png", cellDiv.id, "42px");
+                  player_pos = [c, r];
+                  break;
+        case 'gp': addImg("player2.png", cellDiv.id, "42px");
+                   player_pos = [c, r];
+                   break;
+        case 'gc': addImg("crate.png", cellDiv.id, "42px");
+                   cellDiv.style.width = "40px";
+                   cellDiv.style.height = "40px";
+                   cellDiv.style.border = "1px solid gold";
+                   break;
       }
     }
   }
+}
+
+function move(direction) {
+  var destination = [direction[0] + player_pos[0], direction[1] + player_pos[1]];
+  var cellDiv = document.getElementById(destination.join("_"));
+  var value = lvl[destination[1]][destination[0]];
+  switch(value) {
+    case 'e':
+      if (lvl[player_pos[1]][player_pos[0]] == "gp") {
+        lvl[player_pos[1]][player_pos[0]] = "g";
+      } else {
+        lvl[player_pos[1]][player_pos[0]] = 'e';
+      }
+      lvl[destination[1]][destination[0]] = "p";
+      break;
+    case 'c':
+      if (lvl[destination[1] + direction[1]][destination[0] + direction[0]] == "g") {
+        lvl[player_pos[1]][player_pos[0]] = 'e';
+        if (lvl[destination[1]][destination[0]] == "gc") {
+          lvl[destination[1]][destination[0]] = "gp";
+        } else {
+          lvl[destination[1]][destination[0]] = "p";
+        }
+        lvl[destination[1] + direction[1]][destination[0] + direction[0]] = "gc";
+      } else if (lvl[destination[1] + direction[1]][destination[0] + direction[0]] == "e") {
+        lvl[player_pos[1]][player_pos[0]] = 'e';
+        lvl[destination[1]][destination[0]] = "p";
+        lvl[destination[1] + direction[1]][destination[0] + direction[0]] = "c";
+      }
+      break;
+    case 'w':
+      break;
+    case 'gc':
+      lvl[player_pos[1]][player_pos[0]] = 'e';
+      lvl[destination[1]][destination[0]] = "gp";
+      lvl[destination[1] + direction[1]][destination[0] + direction[0]] = "c";
+      break;
+    case 'g':
+      lvl[player_pos[1]][player_pos[0]] = 'e';
+      lvl[destination[1]][destination[0]] = "gp";
+      break;
+  }
+  refresh();
 }
--- src/sokoban2/handler.clj
+++ src/sokoban2/handler.clj
@@ -7,25 +7,35 @@
             [compojure.response :as response]
             [hiccup
              [page :refer [html5]]
-             [page :refer [include-js]]]))
+             [page :refer [include-js include-css]]]))
 
 (defn index-page []
   (html5
     [:head
      [:title "GSokoban"]
-     (include-js "/js/main.js")]
+     (include-js "/js/main.js")
+     (include-css "css/main.css")]
     [:body
      [:h1 "GSokoban"]
      ;[:canvas {:id "field" :width 300 :height 300}]
-     [:a {:onclick "shit()" :href "#"} "llama"]
-     (vec (concat [:table {:id "level"}]
-      (let [lvl (sokoban2.levels/generate-level 11 11)]
-        (vec
-          (map (fn [xs]
-                 (vec
-                   (concat [:tr]
-                           (map #(vector :td [:div (name %)]) xs))))
-               lvl)))))]))
+     (let [lvl (sokoban2.levels/generate-level 11 11)
+           width (sokoban2.levels/width lvl)
+           height (sokoban2.levels/height lvl)
+           ids (partition width
+                          (for [x (range width)
+                                y (range height)]
+                            (str x "_" y)))]
+       (vec
+         (concat [:table {:id "level"}]
+                 (sokoban2.levels/vectorize2d
+                   (map (fn [row ids-row]
+                          (concat [:tr]
+                                  (map #(vector :td
+                                                [:div {:id %2} (name %1)])
+                                       row ids-row)))
+                        lvl ids)))))
+     [:input {:id "input" :size "0"}]
+     [:script "shit();"]]))
 
 (defroutes app-routes
   (GET "/" [] (index-page))
